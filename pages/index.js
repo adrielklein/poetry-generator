5982497c08548c3cd006d6526db3da8c782bc925
Adriel Klein <adrielmklein@gmail.com>
2023-01-25 15:40:16 -0600

    work on poem generator app

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,96 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import styles from "./index.module.css";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+const poemTypes = ["haiku", "limirick", "sonnet"];
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [poemPrompt, setPoemPrompt] = useState("");
+  const [selectedPoemType, setSelectedPoemType] = useState(poemTypes[0]);
   const [result, setResult] = useState();
+  const onChangePoemType = useCallback(
+    async (event) => {
+      console.log('did it with', )
+      await setSelectedPoemType(event.value);
+      console.log('about to submit with', selectedPoemType)
+      if (poemPrompt.length > 0) {
+        onSubmit();
+      }
+    },
+    [poemPrompt]
+  );
 
-  async function onSubmit(event) {
-    event.preventDefault();
+  const onSubmit = useCallback(async (event) =>  {
+    event && event.preventDefault();
+    console.log({ selectedPoemType });
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ poemPrompt, poemType: selectedPoemType }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
+      console.log({ data });
 
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
-  }
+  }, [selectedPoemType, poemPrompt])
 
   return (
-    <div>
+    <div className={styles.body}>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Poem Generator</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Poem Generator</h3>
+        <div
+          style={{ display: "flex", alignItems: "center", marginBottom: "8px" }}
+        >
+          Write me a{" "}
+          <Dropdown
+            className={styles.dropdown}
+            style={{ marginLeft: "2px", marginRight: "2px" }}
+            options={poemTypes}
+            onChange={onChangePoemType}
+            value={selectedPoemType}
+            placeholder="Select an option"
+          />
+          about...
+        </div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            placeholder="Tell me anything my dear..."
+            value={poemPrompt}
+            onChange={(e) => setPoemPrompt(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Create poem" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: result ? result.replaceAll("\n", "<br />") : "",
+          }}
+          className={styles.result}
+        />
       </main>
     </div>
   );
