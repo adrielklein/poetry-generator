8a962d511134e76d609c994b6f0b2d0e16b3bc03
Adriel Klein <adrielmklein@gmail.com>
2023-01-26 12:12:39 -0600

    add poem generation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import styles from "./index.module.css";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
@@ -8,58 +8,89 @@ const poemTypes = ["haiku", "limirick", "sonnet"];
 
 export default function Home() {
   const [poemPrompt, setPoemPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedPoemType, setSelectedPoemType] = useState(poemTypes[0]);
-  const [result, setResult] = useState();
+  const [poem, setPoem] = useState();
   const onChangePoemType = useCallback(
     async (event) => {
-      console.log('did it with', )
-      await setSelectedPoemType(event.value);
-      console.log('about to submit with', selectedPoemType)
+      console.log("did it with", event.value);
+      setSelectedPoemType(event.value);
+
+      console.log("about to submit with", event.value);
       if (poemPrompt.length > 0) {
-        onSubmit();
+        generatePoem({ prompt: poemPrompt, type: event.value });
       }
     },
     [poemPrompt]
   );
 
-  const onSubmit = useCallback(async (event) =>  {
-    event && event.preventDefault();
-    console.log({ selectedPoemType });
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ poemPrompt, poemType: selectedPoemType }),
-      });
+  const generatePoem = useCallback(
+    async ({ prompt, type }) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("/api/generate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ poemPrompt: prompt, poemType: type }),
+        });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
+        const data = await response.json();
+        if (response.status !== 200) {
+          throw (
+            data.error ||
+            new Error(`Request failed with status ${response.status}`)
+          );
+        }
+        console.log({ data });
+
+        setPoem(data.result);
+        setIsLoading(false);
+      } catch (error) {
+        // Consider implementing your own error handling logic here
+        console.error(error);
+        alert(error.message);
+        setIsLoading(false);
       }
-      console.log({ data });
+    },
+    [setPoem, setIsLoading]
+  );
 
-      setResult(data.result);
-    } catch (error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+  const onSubmit = useCallback(
+    async (event) => {
+      event && event.preventDefault();
+      console.log({ selectedPoemType });
+      generatePoem({ prompt: poemPrompt, type: selectedPoemType });
+    },
+    [selectedPoemType, poemPrompt]
+  );
+  // Color pallette
+  // return (
+  //   <div>
+  //     <div style={{ height: "100px", width: "100px", background: '#F7F7F7' }} />
+  //     <div style={{ height: "100px", width: "100px", background: '#333333' }} />
+  //     <div style={{ height: "100px", width: "100px", background: '#2C3E50' }} />
+  //     <div style={{ height: "100px", width: "100px", background: '#5C3D91' }} />
+  //     <div style={{ height: "100px", width: "100px", background: '#F5A9A9' }} />
+  //   </div>
+  // );
+  const text = useMemo(() => {
+    if (isLoading) {
+      return "Loading...";
     }
-  }, [selectedPoemType, poemPrompt])
+    return poem ? poem.replaceAll("\n", "<br />") : "";
+  }, [isLoading, poem]);
 
   return (
     <div className={styles.body}>
       <Head>
         <title>Poem Generator</title>
-        <link rel="icon" href="/dog.png" />
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
+        <img src="/logo.png" className={styles.icon} />
         <h3>Poem Generator</h3>
         <div
           style={{ display: "flex", alignItems: "center", marginBottom: "8px" }}
@@ -79,7 +110,7 @@ export default function Home() {
           <input
             type="text"
             name="animal"
-            placeholder="Tell me anything my dear..."
+            placeholder=""
             value={poemPrompt}
             onChange={(e) => setPoemPrompt(e.target.value)}
           />
@@ -87,7 +118,7 @@ export default function Home() {
         </form>
         <div
           dangerouslySetInnerHTML={{
-            __html: result ? result.replaceAll("\n", "<br />") : "",
+            __html: text,
           }}
           className={styles.result}
         />
